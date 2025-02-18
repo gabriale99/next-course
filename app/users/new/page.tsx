e0cafc80146f78ac34b667c80606c8675f6a92af
gabriale99 <gabriale99@gmail.com>
2025-02-18 02:15:04 -0800

    Programmatic Navigation

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,16 @@
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const NewUserPage = () => {
-  return <div>NewUserPage</div>;
+  // Imports from next/navigation
+  const router = useRouter();
+
+  return (
+    <button onClick={() => router.push("/users")} className="btn btn-primary">
+      Create
+    </button>
+  );
 };
 
 export default NewUserPage;
