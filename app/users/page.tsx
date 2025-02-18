a33e4419bd5651ec10410bd23bd7ab9b9f71bb44
gabriale99 <gabriale99@gmail.com>
2025-02-17 23:59:43 -0800

    Static and Dynamic Rendering

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,14 +7,17 @@ interface User {
 
 const UsersPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 },
+    cache: "no-store",
   });
   // Caching only works with fetch in Next.js
   const users: User[] = await res.json();
+  // If there are static data in this page, Nextjs will render it in build time statically
+  // Otherwise, if Nextjs sees dynamic data (data that refreshes after a period of time), it renders it dynamically in request time
 
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
