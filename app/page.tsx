bc2596726a29a274a5835e44fe2f9f8e0fb35dd5
gabriale99 <gabriale99@gmail.com>
2025-02-18 02:10:42 -0800

    Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main>
       <h1>Hello World</h1>
       <Link href="/users">Users</Link>
+      {/* 1. Only downloads the content of the target page | 2. Pre-fetches links that are in the viewport | 3. Caches pages on the client */}
       <ProductCard />
     </main>
   );
