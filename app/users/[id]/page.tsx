03cbf39a7a98db23d2dc91b22ae87fd0e942eecf
gabriale99 <gabriale99@gmail.com>
2025-02-18 02:29:30 -0800

    Handling Not Found Errors

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { notFound } from "next/navigation";
 import React from "react";
 
 interface Props {
@@ -6,6 +7,9 @@ interface Props {
 
 // Route params only works on page
 const UserDetailPage = ({ params: { id } }: Props) => {
+  if (id > 10) {
+    notFound();
+  }
   return <div>UserDetailPage {id}</div>;
 };
 
