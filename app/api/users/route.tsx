608a23fd7f65e9b56b6538f6a172eb11339efac8
gabriale99 <gabriale99@gmail.com>
2025-02-22 21:01:49 -0800

    Creating Data

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -9,14 +9,31 @@ export async function GET(request: NextRequest) {
 }
 
 export async function POST(request: NextRequest) {
-  const body = await request.json();
   // Validate
   // If invalid, return 400
   // else, return
+
+  const body = await request.json();
   const validation = schema.safeParse(body);
+
   if (!validation.success) {
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user) {
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+  }
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
