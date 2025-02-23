5d374045bc157d0bf0dfcd09edf371276f8196b4
gabriale99 <gabriale99@gmail.com>
2025-02-22 17:34:31 -0800

    Creating an Object

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -7,3 +7,15 @@ export function GET(request: NextRequest) {
     { id: 2, name: "Jane Smith" },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  // Validate
+  // If invalid, return 400
+  // else, return
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  }
+
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
