9caab598f1831b66e972b4b0be3f824f2d339873
gabriale99 <gabriale99@gmail.com>
2025-02-22 17:39:22 -0800

    Updating an Object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -14,3 +14,26 @@ export function GET(
 
   return NextResponse.json({ id: 1, name: "Gabriel" });
 }
+
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Validate the request body
+  // If invalid, return 400
+  // Fetch the user with the given id
+  // If doesn't exist, return 404 error
+  // Update the user
+  // Return the updated user
+
+  const body = await request.json();
+  if (!body.name) {
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  }
+
+  if (params.id > 10) {
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  }
+
+  return NextResponse.json({ id: 1, body: body.name });
+}
