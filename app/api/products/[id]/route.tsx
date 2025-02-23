c04328dd4bedafb4cda82ed9d19472cace171e18
gabriale99 <gabriale99@gmail.com>
2025-02-22 21:23:10 -0800

    Exercise- Storing Products in DB

--- app/api/products/[id]/route.tsx
+++ app/api/products/[id]/route.tsx
@@ -1,9 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 import schema from "../schema";
+import { prisma } from "@/prisma/client";
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const body = await request.json();
   const validation = schema.safeParse(body);
@@ -11,20 +12,40 @@ export async function PUT(
     return NextResponse.json(validation.error.errors, { status: 400 });
   }
 
-  if (params.id > 10) {
+  const product = await prisma.product.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!product) {
     return NextResponse.json({ error: "Product not found" }, { status: 404 });
   }
 
-  return NextResponse.json({ id: 1, ...body });
+  const updatedProduct = await prisma.product.update({
+    where: { id: product.id },
+    data: {
+      name: body.name,
+      price: body.price,
+    },
+  });
+
+  return NextResponse.json(updatedProduct);
 }
 
 export async function DELETE(
   request: NextRequest,
   { params }: { params: { id: number } }
 ) {
-  if (params.id > 10) {
+  const product = await prisma.product.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!product) {
     return NextResponse.json({ error: "Product not found" }, { status: 404 });
   }
+
+  await prisma.product.delete({
+    where: { id: product.id },
+  });
 
   return NextResponse.json({});
 }
