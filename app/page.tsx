6f0470542deaac270bf838f4f06616801d67a9d6
gabriale99 <gabriale99@gmail.com>
2025-02-25 03:00:54 -0800

    Optimizing Images

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,18 @@
-import Link from "next/link";
-import ProductCard from "./components/ProductCard";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import great from "@/public/images/Great ones1.png";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions);
-
   return (
-    <main>
-      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
-      <Link href="/users">Users</Link>
-      {/* 1. Only downloads the content of the target page | 2. Pre-fetches links that are in the viewport | 3. Caches pages on the client */}
-      <ProductCard />
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="a great photo"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority // z-index
+      />
     </main>
   );
 }
