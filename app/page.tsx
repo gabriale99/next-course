dced4f7b0f22d5a1ff26f64838564be313c839c0
gabriale99 <gabriale99@gmail.com>
2025-02-24 22:25:28 -0800

    Acessing Session on the Server

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
   return (
     <main>
-      <h1>Hello World</h1>
+      <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
       <Link href="/users">Users</Link>
       {/* 1. Only downloads the content of the target page | 2. Pre-fetches links that are in the viewport | 3. Caches pages on the client */}
       <ProductCard />
